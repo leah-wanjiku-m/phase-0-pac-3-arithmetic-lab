c3283ff604abb93d25b4e18a36bdbdc674af00ac
leah wanjiku <leagm22@gmail.com>
2022-11-18 11:35:08 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(number) {
+    return (number + number);
+}
+function subtract(number) {
+    return (number - number);
+}
+function multiply(number) {
+    return (number * number);
+}
+function divide(number) {
+    return (number / number);
+}
+function add(a,b) {
+    return (a+b);
+}
+function subtract( a,b) {
+    return (a-b);
+}
+function multiply(a,b) {
+    return (a * b);
+}
+function divide(a,b) {
+    return (a / b);
+}
+function increment(n) {
+    return (++ n);
+}
+function decrement(n) {
+    return (-- n);
+}
+function makeInt(n) {
+    return (parseInt(n, 10));
+}
+function preserveDecimal(n) {
+    return (parseFloat(n));
+}
